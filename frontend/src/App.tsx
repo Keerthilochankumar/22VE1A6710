fbafcab08b8cf0c0d0a2039e013537c8c4cf873d
Keerthilochankumar <keerthi.lochans@gmail.com>
2025-07-18 14:22:13 +0530

    changer routes in frontend and add screenshorts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,9 +10,7 @@ function App() {
     <>
       <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Welcome to URL Shortener</div>} />
-        <Route path="/create" element={<Shorturl />} />
-        <Route path="/get" element={<div>Get URL Details Page</div>} />
+        <Route path="/" element={<Shorturl />} />
       </Routes>
       </BrowserRouter>
     </>
